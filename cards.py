6b75ba3387d534d217c5d2fd27b9309afc4c9cde
Deivid-U <deividu@nycstudents.net>
2025-03-14 10:16:19 -0400

    dfsgtetay I DID IT
    ewsdfarsegw5e

--- cards.py
+++ cards.py
@@ -5,23 +5,24 @@ class Teacher:
         finished = False
         cards = {}
         while finished == False:
-            if input("type 'YES' if you would like to make a card") == 'YES':
-                cards[input('Type in a question: ')] = input('Type in the answer: ')
+            if input("type 'YES' if you would like to make a card ") == 'YES':
+                cards[input('Type in the question: ')] = input('Type in the answer: ')
             else:
                 finished = True
-        with open(FlashCards.json) as file:
-            json.dump(Ydfjisdoisjfhadjaiojf)
+        with open("FlashCards.json", "w") as file:
+            json.dump(cards, file, indent = 4)
 
 
 class Student:
     def answer_questions():
         streak = 0
         points = 0
-        with open(FlashCards.json) as file:
-            for card in file:
-                print(f'{card.key()}')
-                answer = input('What is the answer?')
-                if answer == card.value():
+        with open("FlashCards.json", "r") as file:
+            studentcards = dict(json.load(file))
+            for card in studentcards:
+                print(f'{card}')
+                answer = input('What is the answer? ')
+                if answer == studentcards[card]:
                     streak += 1
                     points += (10 + streak)
                     print(f'CORRECT! You now have {points} points and a streak of {streak}!')
@@ -30,7 +31,19 @@ class Student:
                     print(f'INCORRECT! The correct answer was "{card.value()}. Your streak has been reset to 0.')
                 print(f'You have answered every index card. Your final score was: {points}')
 
-roledecided = False
+while True:
 
-while roledecided == False:
-    input('Type 1 if you are a Teacher, Type 2 if you are a student.')
+    roledecided = False
+
+    while roledecided == False:
+        response = input('Type 1 if you are a Teacher, Type 2 if you are a student. ')
+        if response == '1':
+            Teacher.make_cards() 
+            roledecided = True
+        elif response == '2':
+            Student.answer_questions()
+            roledecided = True
+        else:
+            print('Input invalid')
+
+

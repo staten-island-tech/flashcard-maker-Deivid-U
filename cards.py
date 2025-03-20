c886ac66572ad3527ba22e1f54c0909b0551f2c9
Deivid-U <deividu@nycstudents.net>
2025-03-20 10:16:24 -0400

    34q

--- cards.py
+++ cards.py
@@ -5,7 +5,7 @@ class Teacher:
         finished = False
         cards = {}
         while finished == False:
-            if input("type 'YES' if you would like to make a card ") == 'YES':
+            if input("type 'YES' if you would like to make a card ") == 'YES': 
                 cards[input('Type in the question: ')] = input('Type in the answer: ')
             else:
                 finished = True
@@ -18,31 +18,31 @@ class Student:
         streak = 0
         points = 0
         with open("FlashCards.json", "r") as file:
-            studentcards = json.load(file)
-            for card in studentcards:
-                print(f'{card}')
-                answer = input('What is the answer? ')
-                if answer == studentcards[card]:
-                    points += (10 + streak)
-                    streak += 1
-                    print(f'CORRECT! You now have {points} points and a streak of {streak}!')
-                else:
-                    streak = 0
-                    print(f'INCORRECT! The correct answer was "{card.value()}. Your streak has been reset to 0.')
-                print(f'You have answered every index card. Your final score was: {points}')
+            student_cards = json.load(file)
+        for card in student_cards:
+            print(f'{card}')
+            answer = input('What is the answer? ')
+            if answer == student_cards[card]:
+                points += (10 + streak)
+                streak += 1
+                print(f'CORRECT! You now have {points} points and a streak of {streak}!')
+            else:
+                streak = 0
+                print(f'INCORRECT! The correct answer was "{card.value()}. Your streak has been reset to 0.')
+            print(f'You have answered every index card. Your final score was: {points}') 
 
 while True:
 
-    roledecided = False
+    role_decided = False
 
-    while roledecided == False:
+    while role_decided == False:
         response = input('Type 1 if you are a Teacher, Type 2 if you are a student. ')
         if response == '1':
             Teacher.make_cards() 
-            roledecided = True
+            role_decided = True
         elif response == '2':
             Student.answer_questions()
-            roledecided = True
+            role_decided = True
         else:
             print('Input invalid')
 
